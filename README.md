59f64062471b91fe26fd2096559635541a3f2dc8
João Vitor Veronese Vieira <joao.veronese@timeprimo.com>
2022-02-27 14:07:57 -0300

    feat: props with Compiler Macros

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 - O erro que não permite `any` como Type inference provém de `strict: true` no `tsconfig.json`
 - Type Inference também funciona muito bem para funções que são criadas inline com um argumento
   - Ao extrair a lógica para outro método, por vezes, precisamos tipar manualmente os parâmetros
+
+- defineProps is a type of function called **Compiler Macro**
+  - _Compiler macros_ are functions that only get executed in compile-time
+  - Also, no need to be imported
+  - And, the most important thing: the compiler macro can be used only within the script setup block
--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { computed, onMounted, reactive, ref } from 'vue';
-import fetchCount from './services/fetchCount';
+import { reactive } from 'vue';
+import Counter from './components/Counter.vue';
 
 // Note: this interface is for make our code more readable
 interface AppInfo {
@@ -8,33 +8,10 @@ interface AppInfo {
   slogan: string;
 }
 
-// Note: w/ fetchCount example, we saw wront Type Inference (and force manually the correct way)
-const count = ref<number | null>(null);
-
-// Note: for demonstration, this computed value return a value
-const nextCount = computed(() => {
-  if (count.value !== null) {
-    return count.value + 1;
-  }
-  return null;
-});
-
 const appInfo: AppInfo = reactive({
   name: 'Counter',
   slogan: 'an app you can count on',
 });
-
-onMounted(() => {
-  fetchCount((initialCount) => {
-    count.value = initialCount;
-  });
-});
-
-function addCount(num: number) {
-  if (count.value !== null) {
-    count.value += num;
-  }
-}
 </script>
 
 <template>
@@ -43,6 +20,5 @@ function addCount(num: number) {
     <h1>{{ appInfo.name }}</h1>
     <h2>{{ appInfo.slogan }}</h2>
   </div>
-  <p>{{ count }}</p>
-  <button @click="addCount(1)">Add</button>
+  <counter :limit="10" :alert-message-on-limit="'can not go any higher'" />
 </template>
